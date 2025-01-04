38c69d7cbba27ff437ba94bcfbe12c69c1eef5ec
emibonezzi <emibonezzi@gmail.com>
2025-01-04 17:19:27 -0500

    refactor endpoints, add optional parameter on get posts to get single post

--- index.js
+++ index.js
@@ -7,20 +7,28 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 
 // get blog posts
-app.get("/api/get-posts", connectToDb, async (req, res) => {
-  // retrieve posts
-  try {
-    const posts = await req.dbClient.query("SELECT * FROM posts");
-    res.status(200).json(posts.rows);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: `Error in retrieving posts`, message: error.message });
+app.get("/blog/posts/:id?", connectToDb, async (req, res) => {
+  // if post id is passed
+  if (req.params.id) {
+    const post = await req.dbClient.query(
+      `SELECT * FROM posts WHERE post_id=${req.params.id}`
+    );
+    res.status(200).json(post.rows);
+  } else {
+    // retrieve ALL posts
+    try {
+      const posts = await req.dbClient.query("SELECT * FROM posts");
+      res.status(200).json(posts.rows);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: `Error in retrieving posts`, message: error.message });
+    }
   }
 });
 
 // add new blog post
-app.post("/api/new-post", connectToDb, async (req, res) => {
+app.post("/blog/posts", connectToDb, async (req, res) => {
   // validate post
   const result = postSchema.validate(req.body);
 
@@ -44,6 +52,9 @@ app.post("/api/new-post", connectToDb, async (req, res) => {
     }
   }
 });
+
+// update post
+app.patch("/blog/posts/:id", connectToDb, async (req, res) => {});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
