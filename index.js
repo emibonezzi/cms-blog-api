56e66c1b21e7b1b602a5321cbaf18721b3e90344
emibonezzi <emibonezzi@gmail.com>
2025-01-04 17:53:43 -0500

    add update post

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ app.get("/blog/posts/:id?", connectToDb, async (req, res) => {
         .json({ error: `Error in retrieving posts`, message: error.message });
     }
   }
+
+  await req.dbClient.end();
 });
 
 // add new blog post
@@ -51,10 +53,56 @@ app.post("/blog/posts", connectToDb, async (req, res) => {
         .json({ error: "Error in saving post", message: error.message });
     }
   }
+
+  await req.dbClient.end();
 });
 
 // update post
-app.patch("/blog/posts/:id", connectToDb, async (req, res) => {});
+app.patch("/blog/posts/:id?", connectToDb, async (req, res) => {
+  // if post id is passed
+  if (req.params.id) {
+    // look for post to update
+    const post = await req.dbClient.query(
+      `SELECT * FROM posts WHERE post_id=${req.params.id}`
+    );
+    if (!post.rows.length === 0) {
+      // if post not found
+      res.status(400).json({
+        error: `Error in updating post`,
+        message: "post_id doesn't exist",
+      });
+    } else {
+      // validate post
+      const result = postSchema.validate(req.body);
+      if (result.error) {
+        res.status(500).json({
+          error: "Post not valid",
+          message: result.error.details[0].message,
+        });
+      } else {
+        // update post in db
+        try {
+          const post = await req.dbClient.query(
+            `UPDATE posts SET title = '${req.body.title}', body = '${req.body.body}', modified_at = 'now()' WHERE post_id = ${req.params.id} RETURNING *`
+          );
+
+          res.status(200).json(post.rows);
+        } catch (error) {
+          res
+            .status(400)
+            .json({ error: "Error in updating post", message: error.message });
+        }
+      }
+    }
+  } else {
+    res.status(400).json({
+      error: `Error in updating post`,
+      message: "Please provide a post_id",
+    });
+  }
+
+  await req.dbClient.end();
+});
 
 // Error handling middleware
 app.use((err, req, res, next) => {
