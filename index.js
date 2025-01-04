03895e65ea298010ec339c33dcf0f4c8c501634e
emibonezzi <emibonezzi@gmail.com>
2025-01-04 15:01:45 -0500

    add get-posts endpoint

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ app.get("/api/get-posts", async (req, res) => {
   } catch (error) {
     res.status(500).send(`Error in connecting to db:${error.message}`);
   }
+
+  // retrieve posts
+  try {
+    const posts = await client.query("SELECT * FROM posts");
+    res.status(200).json(posts.rows);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: `Error in retrieving posts`, message: error.message });
+  }
 });
 
 // start web server
