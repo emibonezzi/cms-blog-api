eb612c42357811e2c82303fc7663091c164d37c4
emibonezzi <emibonezzi@gmail.com>
2025-01-06 18:02:53 -0500

    add delete endpoint

--- index.js
+++ index.js
@@ -76,6 +76,25 @@ app.patch(
   }
 );
 
+// delete endpoint
+app.delete("/blog/posts/:id", connectToDb, async (req, res, next) => {
+  if (req.params.id) {
+    const post = await searchPostId(req.params.id, req.dbClient);
+    if (post.rows.length === 0) {
+      next(new MyCustomError("Post id doesn't exist", 400));
+    } else {
+      const post = await req.dbClient.query(
+        `DELETE FROM posts WHERE post_id = ${req.params.id} RETURNING *`
+      );
+      res.status(200).json({ status: "deleted", post: post.rows });
+    }
+  } else {
+    next(new MyCustomError("Please provide a post id", 400));
+  }
+
+  await req.dbClient.end();
+});
+
 // Error handling middleware
 app.use((err, req, res, next) => {
   res.status(err.statusCode).json({ message: err.message });
